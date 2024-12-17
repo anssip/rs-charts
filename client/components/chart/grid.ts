a2df4997f2e3b2d4656198bb85f5552382d3a052
Anssi Piirainen <api@iki.fi>
2024-12-17 16:36:55 +0200

    Cleanups2 (#8)
    * Refactor timeline iterating
    
    * progress
    
    * better now
    
    * Fix six hour labels
    
    * Fix live subscription
    
    * Use iterateTimeline for the grid
    
    * Refactor candles drawing
    
    * Update readme

--- client/components/chart/grid.ts
+++ client/components/chart/grid.ts
@@ -1,21 +1,20 @@
 import {
-  formatTime,
-  getFirstLabelTimestamp,
-  getGridInterval,
+  iterateTimeline,
   priceToY,
+  getTimelineMarks,
 } from "../../util/chart-util";
 import { getPriceStep } from "../../util/price-util";
 import { DrawingContext, Drawable } from "./drawing-strategy";
 
 export class HairlineGrid implements Drawable {
-  // TODO: make this zoomable
   public draw(context: DrawingContext): void {
     const {
       ctx,
       chartCanvas: canvas,
       data,
       priceRange,
-      axisMappings: { timeToX },
+      viewportStartTimestamp,
+      viewportEndTimestamp,
     } = context;
     const dpr = window.devicePixelRatio ?? 1;
 
@@ -23,43 +22,33 @@ export class HairlineGrid implements Drawable {
       start: priceRange.min,
       end: priceRange.max,
     });
-    const gridInterval = getGridInterval(data.getGranularity());
 
-    const firstGridTimestamp = getFirstLabelTimestamp(
-      context.viewportStartTimestamp,
-      data.getGranularity()
-    );
+    ctx.strokeStyle = "rgba(180, 180, 180, 1)";
+    ctx.setLineDash([3, 3]);
+    ctx.lineWidth = 0.5;
 
-    ctx.strokeStyle = "#ddd";
-    ctx.setLineDash([5, 5]);
-    ctx.lineWidth = 1;
+    iterateTimeline({
+      callback: (x: number, timestamp: number) => {
+        const { tickMark: tickkMark, dateChange } = getTimelineMarks(
+          new Date(timestamp),
+          data.getGranularity()
+        );
+        const doDraw =
+          data.getGranularity() === "ONE_DAY" ? dateChange : tickkMark;
 
-    ctx.font = `${6 * dpr}px Arial`;
-    ctx.fillStyle = "#999";
-    ctx.textAlign = "center";
+        if (doDraw && x >= 0 && x <= canvas.width) {
+          ctx.beginPath();
+          ctx.moveTo(x, 0);
+          ctx.lineTo(x, canvas.height / dpr);
+          ctx.stroke();
+        }
+      },
+      granularity: data.getGranularity(),
+      viewportStartTimestamp,
+      viewportEndTimestamp,
+      canvasWidth: canvas.width / dpr,
+    });
 
-    // Draw vertical lines until we exceed viewport end
-    for (
-      let timestamp = firstGridTimestamp;
-      timestamp <= context.viewportEndTimestamp + gridInterval; // Add one extra interval to handle partial visibility
-      timestamp += gridInterval
-    ) {
-      const x = timeToX(timestamp) / dpr;
-
-      // Only draw if the line is within the visible area
-      if (x >= 0 && x <= canvas.width / dpr) {
-        // Draw grid line
-        ctx.beginPath();
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, canvas.height / dpr);
-        ctx.stroke();
-
-        // draw time label at the top of grid line
-        ctx.fillText(formatTime(new Date(timestamp)), x, 10);
-      }
-    }
-
-    // Draw horizontal lines for every 10% price change
     const priceStep = getPriceStep(priceRange.range);
     const firstPriceGridLine =
       Math.floor(priceRange.min / priceStep) * priceStep;
@@ -78,5 +67,9 @@ export class HairlineGrid implements Drawable {
         ctx.stroke();
       }
     }
+  }
+
+  public destroy(): void {
+    // No cleanup needed
   }
 }
