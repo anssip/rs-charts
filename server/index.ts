93f644480ead9beec86041548607798149c9214b
Anssi Piirainen <api@iki.fi>
2024-12-31 14:47:16 +0200

    Fix merge issue

--- server/index.ts
+++ server/index.ts
@@ -23,15 +23,6 @@ const allowedOrigins = [
   "https://spot-ws.webflow.io",
 ];
 
-const port = process.env.PORT || 8080;
-
-const allowedOrigins = [
-  "http://localhost:8080",
-  "http://127.0.0.1:8080",
-  "https://spotcanvas.com",
-  "https://chart-api.spotcanvas.com",
-];
-
 const server = serve({
   port: port,
   async fetch(req) {
